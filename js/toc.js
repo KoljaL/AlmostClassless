e4b581b7a3c3c0a62c05b239f425bfcc015222d9
Kolja <lasar@rasal.de>
2023-01-16 00:48:44 +0100

    init

---
+++ js/toc.js
@@ -0,0 +1,181 @@
+function markerTOC() {
+    var toc = document.querySelector('.toc');
+    var tocPath = document.querySelector('.toc-marker path');
+    var tocItems, lastPathStart, lastPathEnd;
+    // Factor of screen size that the element must cross before it's considered visible
+    var TOP_MARGIN = 0.1,
+        BOTTOM_MARGIN = 0.2;
+    var pathLength;
+    window.addEventListener('resize', drawPath, false);
+    window.addEventListener('scroll', sync, false);
+    drawPath();
+    function drawPath() {
+        tocItems = [].slice.call(toc.querySelectorAll('li'));
+        // Cache element references and measurements
+        tocItems = tocItems.map(function (item) {
+            var anchor = item.querySelector('a');
+            var target = document.getElementById(anchor.getAttribute('href').slice(1));
+            return {
+                listItem: item,
+                anchor: anchor,
+                target: target,
+            };
+        });
+
+        // Remove missing targets
+        tocItems = tocItems.filter(function (item) {
+            return !!item.target;
+        });
+
+        var path = [];
+        var pathIndent;
+        tocItems.forEach(function (item, i) {
+            var x = item.anchor.offsetLeft - 5,
+                y = item.anchor.offsetTop,
+                height = item.anchor.offsetHeight;
+            if (i === 0) {
+                path.push('M', x, y, 'L', x, y + height);
+                item.pathStart = 0;
+            } else {
+                // Draw an additional line when there's a change in indent levels
+                if (pathIndent !== x) path.push('L', pathIndent, y);
+                path.push('L', x, y);
+                // Set the current path so that we can measure it
+                tocPath.setAttribute('d', path.join(' '));
+                item.pathStart = tocPath.getTotalLength() || 0;
+                path.push('L', x, y + height);
+            }
+            pathIndent = x;
+            tocPath.setAttribute('d', path.join(' '));
+            item.pathEnd = tocPath.getTotalLength();
+        });
+        pathLength = tocPath.getTotalLength();
+        sync();
+    }
+
+    function sync() {
+        var windowHeight = window.innerHeight;
+        var pathStart = pathLength,
+            pathEnd = 0;
+        var visibleItems = 0;
+        tocItems.forEach(function (item) {
+            var targetBounds = item.target.getBoundingClientRect();
+            if (targetBounds.bottom > windowHeight * TOP_MARGIN && targetBounds.top < windowHeight * (1 - BOTTOM_MARGIN)) {
+                pathStart = Math.min(item.pathStart, pathStart);
+                pathEnd = Math.max(item.pathEnd, pathEnd);
+                visibleItems += 1;
+                item.listItem.classList.add('visible');
+            } else {
+                item.listItem.classList.remove('visible');
+            }
+        });
+
+        // Specify the visible path or hide the path altogether
+        // if there are no visible items
+        if (visibleItems > 0 && pathStart < pathEnd) {
+            if (pathStart !== lastPathStart || pathEnd !== lastPathEnd) {
+                tocPath.setAttribute('stroke-dashoffset', '1');
+                tocPath.setAttribute('stroke-dasharray', '1, ' + pathStart + ', ' + (pathEnd - pathStart) + ', ' + pathLength);
+                tocPath.setAttribute('opacity', 1);
+            }
+        } else {
+            tocPath.setAttribute('opacity', 0);
+        }
+        lastPathStart = pathStart;
+        lastPathEnd = pathEnd;
+    }
+}
+
+async function generateTOC() {
+    var contentContainer = document.querySelector('main');
+    var headings = contentContainer.querySelectorAll('h1.forTOC, h2.forTOC, h3.forTOC, h4.forTOC, h5.forTOC, h6.forTOC');
+    var tocContainer = document.querySelector('.toc');
+    var ul = document.createElement('ul');
+    // Loop through the headings NodeList
+    for (i = 0; i <= headings.length - 1; i++) {
+        var id = headings[i].innerHTML.toLowerCase().replace(/ /g, '-');
+        var level = headings[i].localName.replace('h', '');
+        var title = headings[i].innerHTML;
+        headings[i].setAttribute('id', id);
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.setAttribute('href', '#' + id);
+        a.innerHTML = title;
+        // Create the hierarchy
+        if (level == 1) {
+            li.appendChild(a);
+            ul.appendChild(li);
+        } else if (level == 2) {
+            child = document.createElement('ul');
+            li.appendChild(a);
+            child.appendChild(li);
+            ul.appendChild(child);
+        } else if (level == 3) {
+            grandchild = document.createElement('ul');
+            li.appendChild(a);
+            grandchild.appendChild(li);
+            child.appendChild(grandchild);
+        } else if (level == 4) {
+            great_grandchild = document.createElement('ul');
+            li.append(a);
+            great_grandchild.appendChild(li);
+            grandchild.appendChild(great_grandchild);
+        }
+    }
+    tocContainer.appendChild(ul);
+    // return true;
+    markerTOC();
+}
+
+// add CSS to header
+var style = document.createElement('style');
+style.innerHTML = /*CSS*/ `
+.toc {
+  position: fixed;
+  left: 0rem;
+  top: 5em;
+  padding: .5em;
+  width: calc(max-content + 1em);
+  line-height: 1;
+  border:1px solid var(--secondary);
+    border-radius: 5px;
+  background-color: var(--secondary-bg);
+}
+
+.toc ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  padding-left: 0em;
+}
+
+.toc ul ul {
+  padding-left: 1em;
+}
+
+.toc li a {
+  display: inline-block;
+  color: var(--primary);
+  text-decoration: none;
+  transition: all 0.3s cubic-bezier(0.230, 1.000, 0.320, 1.000);
+}
+
+.toc li.visible>a {
+  color: var(--secondary);
+  transform: translate(5px);
+}
+
+.toc-marker {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+
+.toc-marker path {
+  transition: all 0.3s ease;
+}`;
+
+document.getElementsByTagName('HEAD')[0].appendChild(style);
---
+++ js/toggleTheme.js
@@ -0,0 +1,108 @@
+// add CSS to header
+var style = document.createElement('style');
+style.innerHTML = /*CSS*/ `
+.theme-toggle.theme-toggle--reversed .theme-toggle__classic {
+  transform: scale(-1, 1);
+}
+
+.theme-toggle__classic path {
+  transition-timing-function: cubic-bezier(0, 0, 0.15, 1.25);
+  transform-origin: center;
+  transition-duration: calc(var(--theme-toggle__classic--duration) * 0.8);
+}
+
+.theme-toggle__classic g path {
+  transition-property: opacity, transform;
+  transition-delay: calc(var(--theme-toggle__classic--duration) * 0.2);
+}
+
+.theme-toggle__classic :first-child path {
+  transition-property: transform, d;
+}
+
+.theme-toggle input[type="checkbox"]:checked~.theme-toggle__classic g path,
+.theme-toggle--toggled:not(label).theme-toggle .theme-toggle__classic g path {
+  transform: scale(0.5) rotate(45deg);
+  opacity: 0;
+  transition-delay: 0s;
+}
+
+.theme-toggle input[type="checkbox"]:checked~.theme-toggle__classic :first-child path,
+.theme-toggle--toggled:not(label).theme-toggle .theme-toggle__classic :first-child path {
+  d: path("M-12 5h30a1 1 0 0 0 9 13v24h-39Z");
+  transition-delay: calc(var(--theme-toggle__classic--duration) * 0.2);
+}
+
+@supports not (d: path("")) {
+
+  .theme-toggle input[type="checkbox"]:checked~.theme-toggle__classic :first-child path,
+  .theme-toggle--toggled:not(label).theme-toggle .theme-toggle__classic :first-child path {
+    transform: translate3d(-12px, 10px, 0);
+  }
+}
+
+.theme-toggle {
+  border: none;
+  background: 0 0;
+  cursor: pointer;
+  margin-right: .5rem;
+}
+
+.theme-toggle input[type="checkbox"] {
+  display: none;
+}
+
+.theme-toggle .theme-toggle-sr {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border-width: 0;
+}
+`;
+
+document.getElementsByTagName('HEAD')[0].appendChild(style);
+
+document.querySelector('.theme-toggle').innerHTML = /*HTML*/ `
+<input type="checkbox" />
+<span class="theme-toggle-sr">Toggle theme</span>
+<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="2em" height="2em" fill="var(--secondary-light)" stroke-linecap="round" class="theme-toggle__classic" viewBox="0 0 32 32">
+  <clipPath id="theme-toggle__classic__cutout">
+    <path d="M0-5h30a1 1 0 0 0 9 13v24H0Z" />
+  </clipPath>
+  <g clip-path="url(#theme-toggle__classic__cutout)">
+    <circle cx="16" cy="16" r="9.34" />
+    <g stroke="var(--secondary-light)" stroke-width="1.5">
+      <path d="M16 5.5v-4" />
+      <path d="M16 30.5v-4" />
+      <path d="M1.5 16h4" />
+      <path d="M26.5 16h4" />
+      <path d="m23.4 8.6 2.8-2.8" />
+      <path d="m5.7 26.3 2.9-2.9" />
+      <path d="m5.8 5.8 2.8 2.8" />
+      <path d="m23.4 23.4 2.9 2.9" />
+    </g>
+  </g>
+</svg>`;
+
+var toggle = document.querySelector('.theme-toggle');
+const faviconEl = document.querySelector('link[rel="icon"]');
+// var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+// if (storedTheme)
+//   document.documentElement.setAttribute('data-theme', storedTheme)
+
+toggle.onchange = function () {
+    var currentTheme = document.documentElement.getAttribute('data-theme') || 'light';
+    var targetTheme = 'light';
+    faviconEl.setAttribute('href', 'img/favicon-light.png');
+    if (currentTheme === 'light') {
+        targetTheme = 'dark';
+        faviconEl.setAttribute('href', 'img/favicon-dark.png');
+    }
+    document.documentElement.setAttribute('data-theme', targetTheme);
+    // localStorage.setItem('theme', targetTheme);
+};
